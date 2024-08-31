89c1d5696f7f59a3afd116fb82b7c2d8ad452d3d
Ye-Gao1 <Yeg7311@gmail.com>
2024-08-31 00:53:23 +0000

    Hangman

--- main.py
+++ main.py
@@ -1,116 +1,87 @@
 import discord
-import random
+import requests
 import asyncio
 import os
 
-TOKEN = os.environ['Token']
 intents = discord.Intents.default()
 intents.message_content = True
+Token = os.environ['Token']
 client = discord.Client(intents=intents)
 
-DIRECTIONS = {
-    '⬅️': (-1, 0),
-    '➡️': (1, 0),
-    '⬆️': (0, -1),
-    '⬇️': (0, 1)
-}
+game_in_progress = False
+word_to_guess = ""
+guessed_letters = set()
+attempts_left = 6
 
-GRID_SIZE = 10
-SNAKE_ICON = '🟩'
-FOOD_ICON = '🍎'
-EMPTY_ICON = '⬜'
+def get_random_word():
+    response = requests.get("https://random-word-api.herokuapp.com/word?number=1&length=5")
+    if response.status_code == 200:
+        return response.json()[0]
+    else:
+        return "apple"
 
-class SnakeGame:
-    def __init__(self):
-        self.snake = [(5, 5)]
-        self.direction = DIRECTIONS['➡️']
-        self.food = self.random_food()
-        self.score = 0
+def display_word():
+    return ' '.join(letter if letter in guessed_letters else '_' for letter in word_to_guess)
 
-    def random_food(self):
-        while True:
-            food = (random.randint(0, GRID_SIZE-1), random.randint(0, GRID_SIZE-1))
-            if food not in self.snake:
-                return food
+def display_hangman():
+    stages = [
+        "  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n========="
+    ]
+    return stages[6 - attempts_left]
 
-    def move(self):
-        head_x, head_y = self.snake[0]
-        delta_x, delta_y = self.direction
-        new_head = (head_x + delta_x, head_y + delta_y)
-
-        if (new_head in self.snake or 
-            not (0 <= new_head[0] < GRID_SIZE) or 
-            not (0 <= new_head[1] < GRID_SIZE)):
-            return False
-
-        self.snake.insert(0, new_head)
-        if new_head == self.food:
-            self.food = self.random_food()
-            self.score += 1
-        else:
-            self.snake.pop()
-        return True
-
-    def render(self):
-        grid = [[EMPTY_ICON] * GRID_SIZE for _ in range(GRID_SIZE)]
-        for x, y in self.snake:
-            grid[y][x] = SNAKE_ICON
-        grid[self.food[1]][self.food[0]] = FOOD_ICON
-        return '\n'.join(''.join(row) for row in grid)
+def format_game_state():
+    hangman = display_hangman()
+    word = display_word()
+    guesses = ", ".join(sorted(guessed_letters)) or "None"
+    return f"```\n{hangman}\n\nWord: {word}\nGuessed letters: {guesses}\nAttempts left: {attempts_left}\n```"
 
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user}')
+    print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
+    global game_in_progress, word_to_guess, guessed_letters, attempts_left
+
     if message.author == client.user:
         return
 
-    if message.content.lower() == '!start':
-        game = SnakeGame()
-        embed = discord.Embed(title="Snake Game", description=game.render())
-        embed.add_field(name="Score", value=game.score)
-        game_message = await message.channel.send(embed=embed)
+    if message.content.startswith('!hangman'):
+        if game_in_progress:
+            await message.channel.send("A game is already in progress!")
+        else:
+            game_in_progress = True
+            word_to_guess = get_random_word()
+            guessed_letters = set()
+            attempts_left = 6
+            await message.channel.send("Hangman game started! Guess the 5-letter word.")
+            await message.channel.send(format_game_state())
 
-        for emoji in DIRECTIONS.keys():
-            await game_message.add_reaction(emoji)
+    elif game_in_progress and len(message.content) == 1:
+        guess = message.content.lower()
+        if guess in guessed_letters:
+            await message.channel.send("You already guessed that letter!")
+        elif guess in word_to_guess:
+            guessed_letters.add(guess)
+            await message.channel.send("Correct guess!")
+        else:
+            guessed_letters.add(guess)
+            attempts_left -= 1
+            await message.channel.send("Wrong guess!")
 
-        def check(reaction, user):
-            return user != client.user and str(reaction.emoji) in DIRECTIONS
+        await message.channel.send(format_game_state())
 
-        while True:
-            try:
-                tasks = [
-                    asyncio.create_task(client.wait_for('reaction_add', timeout=0.5, check=check)),
-                    asyncio.create_task(asyncio.sleep(0.5))
-                ]
-                done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+        if set(word_to_guess) <= guessed_letters:
+            await message.channel.send(f"Congratulations! You guessed the word: {word_to_guess}")
+            game_in_progress = False
+        elif attempts_left == 0:
+            await message.channel.send(f"Game over! The word was: {word_to_guess}")
+            game_in_progress = False
 
-                for task in pending:
-                    task.cancel()
-
-                if done:
-                    result = done.pop().result()
-                    if isinstance(result, tuple):
-                        reaction, user = result
-                        game.direction = DIRECTIONS[str(reaction.emoji)]
-                        await game_message.remove_reaction(reaction.emoji, user)
-
-                if not game.move():
-                    await message.channel.send(f"Game over! Final score: {game.score}")
-                    break
-
-                embed = discord.Embed(title="Snake Game", description=game.render())
-                embed.add_field(name="Score", value=game.score)
-                await game_message.edit(embed=embed)
-
-            except asyncio.CancelledError:
-                pass
-            except Exception as e:
-                print(f"An error occurred: {e}")
-                break
-
-        await message.channel.send("Type `!start` to play again!")
-
-client.run(TOKEN)
+client.run(Token)
